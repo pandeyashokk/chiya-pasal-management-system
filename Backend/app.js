270657893fd7e57d109b89b39b417a65f43e128b
pandeyashokk <pandeashok003@gmail.com>
2025-12-30 12:17:15 +0545

    modified url on cors

--- Backend/app.js
+++ Backend/app.js
@@ -20,10 +20,7 @@ app.use(morgan("dev"));
 
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "https://chiya-pasal-mgmt-frontend.onrender.com",
-    ],
+    origin: "http://localhost:5173",
     credentials: true,
   })
 );
